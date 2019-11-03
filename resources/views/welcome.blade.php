6473eba8049dd6d00fe03c543a3f92f5497649ad
fgfdfszxz <fgfdfszxz@163.com>
2019-11-03 01:31:34 +0000

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+			html, body {
+ height: 100%;
+ }
+            body {
+ margin: 0;
+ padding: 0;
+ width: 100%;
+ display: table;
+ font-weight: 100;
+ font-family: "Helvetica Neue", NotoSansHans-Regular,AvenirNext-Regula
+ color:#777;
+ }
+			.container{
+				text-align: center;
+				display: table-cell;
+				vertical-align: middle;
+			}
+            .content {
+                text-align: center;
+				display: inline-block;
+ display: inline-block;	
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+          <div class="content">
+			<div class="title">Hello Laravel! - by Summer</div>
+		  </div>
+        </div>
+    </body>
+</html>
